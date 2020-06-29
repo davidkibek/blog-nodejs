5d524cc02f381c32e1da311732fa19976005c917
davidkibek <kibe02@gmail.com>
2020-06-29 18:30:40 +0300

    crud functions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog')
+const Blog = require('./models/blog');
+const { render } = require('ejs');
 // express app
 
 const app = express();
@@ -18,6 +19,7 @@ app.set('view engine', 'ejs')
 // middleware & static pages
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 
 
 app.get('/', (req, res)=>{
@@ -29,7 +31,7 @@ app.get('/about', (req, res)=>{
 })
 
 app.get('/blogs', (req, res)=>{
-    Blog.find().sort({createdAT: -1})
+    Blog.find().sort({createdAt: -1})
         .then((results)=>{
             res.render('index', {blogs: results})
         })
@@ -39,6 +41,41 @@ app.get('/blogs', (req, res)=>{
 app.get('/blogs/create', (req, res)=>{
     res.render('create')
 })
+
+app.post('/blogs', (req, res)=>{
+    const blog = new Blog(req.body)
+
+    blog.save()
+        .then((result)=>{
+            res.redirect('/blogs')
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+})
+
+app.get('/blogs/:id', (req, res)=>{
+   const id = req.params.id;
+   Blog.findById(id)
+    .then((results)=>{
+        res.render('details',{blog: results})
+    })
+    .catch((err)=>{
+        console.log(err)
+    })   
+})
+
+app.delete('/blogs/:id', (req, res)=>{
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+     .then((results)=>{
+         res.json({redirect: '/blogs'})
+     })
+     .catch((err)=>{
+         console.log(err)
+     })   
+ })
+
 
 app.use((req,res)=>{
     res.status(404).render('404')
