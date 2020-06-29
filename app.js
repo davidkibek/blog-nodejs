0c3053dd2ed88ad36104faf1421ead6fb5f8d8fd
davidkibek <kibe02@gmail.com>
2020-06-29 13:56:38 +0300

    mongodb mongoose and index.ejs

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const Blog = require('./models/blog')
 // express app
 
 const app = express();
 
+//connect to mongodb
+const dbURL = 'mongodb+srv://blog:1234@blog-rx99q.mongodb.net/blog?retryWrites=true&w=majority';
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result)=> app.listen(3000))
+    .catch((err)=>console.log(err))
+
 // register view engine
 
 app.set('view engine', 'ejs')
-
-// listen for requests
-
-app.listen(3000);
 
 // middleware & static pages
 
@@ -18,11 +21,19 @@ app.use(express.static('public'))
 
 
 app.get('/', (req, res)=>{
-    res.render('index')
+    res.redirect('/blogs')
 })
 
 app.get('/about', (req, res)=>{
     res.render('about')
+})
+
+app.get('/blogs', (req, res)=>{
+    Blog.find().sort({createdAT: -1})
+        .then((results)=>{
+            res.render('index', {blogs: results})
+        })
+   
 })
 
 app.get('/blogs/create', (req, res)=>{
